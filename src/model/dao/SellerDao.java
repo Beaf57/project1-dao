d408cc0b9c5dcd65ab0e3df4e8948f7581156647
Beatriz Farias Péres <devbeatrizfarias.bfp@gmail.com>
2026-03-31 16:47:04 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entity.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findBySeller(Integer id);
+	List<Seller> fintAll();
+}
